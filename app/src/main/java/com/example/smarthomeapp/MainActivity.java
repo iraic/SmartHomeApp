c195b2051a7a0f96d3264b0dd832f929b61651e6
Iraic <joseiraic@gmail.com>
2022-02-18 12:59:47 -0600

    Initial commit

---
+++ app/src/main/java/com/example/smarthomeapp/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.smarthomeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+    EditText etUser, etPass;
+    Button bInicio;
+    SharedPreferences sesion;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        etUser = findViewById(R.id.etUser);
+        etPass = findViewById(R.id.etPass);
+        bInicio = findViewById(R.id.bInicio);
+        sesion = getSharedPreferences("sesion",0);
+        bInicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login();
+            }
+        });
+    }
+
+    private void login() {
+        String url = Uri.parse("http://localhost/smarthome/login.php")
+                .buildUpon()
+                .appendQueryParameter("user",etUser.getText().toString())
+                .appendQueryParameter("pass", etPass.getText().toString())
+                .build().toString();
+        JsonObjectRequest peticion = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>(){
+                    @Override
+                    public void onResponse(JSONObject response) {
+                       respuesta();
+                    }
+                }, new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void respuesta(){
+
+    }
+}
